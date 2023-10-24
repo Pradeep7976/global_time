47c1d26be6fe036d57d7b25baa61c1199c6778b9
pradeep <phantompradeep200@gmail.com>
2023-10-24 19:45:07 +0530

    changed the api

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -15,16 +15,14 @@ class WorldTime{
 
     Future<void> getTime() async {
       try{
-        var response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+        var response = await get(Uri.parse('https://www.timeapi.io/api/Time/current/zone?timeZone=$url'));
         Map data = jsonDecode(response.body);
 
-        String datetime=data['utc_datetime'];
-        String offset=data['utc_offset'].toString().substring(1,3);
+        String datetime=data['dateTime'];
 
         DateTime now =DateTime.parse(datetime);
-        now = now.add(Duration(hours:( int.parse(offset)))).add(Duration(minutes: 30));
 
-        isDayTime = now.hour > 6 && now.hour<20 ?true:false;
+        isDayTime = now.hour > 6 && now.hour<19 ?true:false;
 
         time= DateFormat.jm().format(now);
       }catch(e){
